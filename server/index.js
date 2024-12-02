86a46367b2b00a73db149c1ce09e21ce3084953b
Kamal Nayan <sknasylum8454@gmail.com>
2024-12-02 21:26:16 +0530

    feat: added initial files

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import userRoute from "./routes/user.routes.js";
+import { setupDatabase } from "./db/db.js";
+import { seedUser } from "./models/user.model.js";
+
+dotenv.config();
+
+// mongodbConnect();
+setupDatabase();
+seedUser();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", userRoute);
+
+app.listen(5000, () => console.log("✅ Server 5000"));
